360b4fba4a7f4f719c2064ab2fd244c0fb36f81c
Yuriy <39201286+yuri2711@users.noreply.github.com>
2021-04-12 12:22:45 +0300

    Update MainApp.java

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -23,15 +23,15 @@ public class MainApp {
         userService.add(userss2);
 
         List<User> users = userService.listUsers();
-//        for (User user : users) {
-//            System.out.println("Id = " + user.getId());
-//            System.out.println("First Name = " + user.getFirstName());
-//            System.out.println("Last Name = " + user.getLastName());
-//            System.out.println("Email = " + user.getEmail());
-//            System.out.println("car = " + user.getCar().getModel());
-//            System.out.println("car_series = " + user.getCar().getSeries());
-//            System.out.println();
-//        }
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("car = " + user.getCar().getModel());
+            System.out.println("car_series = " + user.getCar().getSeries());
+            System.out.println();
+        }
 
         userService.getUser(8);
 
