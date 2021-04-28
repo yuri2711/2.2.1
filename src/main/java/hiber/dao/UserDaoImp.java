bd41ae7219ed3f791bc723d29029625bc48fea5e
Yuri <79991621127@yandex.ru>
2021-04-28 06:43:46 +0300

    1

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,14 +1,11 @@
 package hiber.dao;
 
-import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
